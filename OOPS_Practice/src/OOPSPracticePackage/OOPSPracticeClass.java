4c7ac73d8ffd0c6c75d3584595342d9473dcc8c0
rohitpardeshi <pardeshirohit080294@gmail.com>
2020-01-12 16:18:40 +0530

    Added AdditionPractice code.

--- OOPS_Practice/src/OOPSPracticePackage/OOPSPracticeClass.java
+++ OOPS_Practice/src/OOPSPracticePackage/OOPSPracticeClass.java
@@ -2,10 +2,21 @@ package OOPSPracticePackage;
 
 public class OOPSPracticeClass {
 	public static void main(String[] args) {
-	
-		StarPattern starPattern=new StarPattern();
+		
+		/*StarPattern starPattern=new StarPattern();
+		
 		starPattern.leftAlignedStarPattern();
 		starPattern.rightAlignedStarPattern();
 		starPattern.triangleStarPattern();
+	
+		Palindrome palindrome=new Palindrome();
+		palindrome.checkPalindromeNumber();
+		
+		ReverseString reverse=new ReverseString();
+		reverse.reverseString();*/
+		
+		AdditionPractice additionPractice=new AdditionPractice();
+		long numbersList[]= {7,2,6,2};
+		additionPractice.addTarget(numbersList,7);
 	}
 }
